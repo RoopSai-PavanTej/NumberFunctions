4deacc2d5468bfb4204cbbb2b14f35060515bc02
RoopSai-PavanTej <60775372+RoopSai-PavanTej@users.noreply.github.com>
2020-02-08 01:36:27 +0530

    Add files via upload

---
+++ Num-functions/Num-functions/Num_functions/testNumbers.py
@@ -0,0 +1,13 @@
+import Numfunctions
+a=Numfunctions.isprime(407)
+print (a)
+
+b=Numfunctions.isodd(51432)
+print(b)
+
+c=Numfunctions.digreverse(995186)
+print(c)
+
+Numfunctions.prteven(123456789)
+
+Numfunctions.prtprime(123456789123456789)
